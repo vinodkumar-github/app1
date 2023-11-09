e3ea483d119cc2dfcae2c2ab1cccb32b6aa39369
vinodkumar-github <corners_overrun_0o@icloud.com>
2023-11-09 21:35:24 +0530

    added css

--- src/Body/Cart.js
+++ src/Body/Cart.js
@@ -1,58 +1,85 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const ulStyle = { 'listtype': "none",
+// CSS styles
+const cartStyle = {
+  maxWidth: '80%',
+  margin: '0 auto',
 };
 
+const listItemStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: '12px',
+};
 
+const checkboxStyle = {
+  transform: 'scale(2)', 
+};
+
+const deleteButtonStyle = {
+  padding: '8px 12px',
+  background: 'red',
+  color: 'white',
+  border: 'none',
+  cursor: 'pointer',
+};
 
 function Cart() {
-    const [goods, setGoods] = useState([
-        {
-        id: 1,
-        name: "Mobile",
-        added: true,
+  const [goods, setGoods] = useState([
+    {
+      id: 1,
+      name: 'Mobile',
+      added: true,
     },
     {
-        id: 2,
-        name: "Laptop",
-        added: true,
+      id: 2,
+      name: 'Laptop',
+      added: true,
     },
     {
-        id: 3,
-        name: "Tablet",
-        added: true,
-    },] );
+      id: 3,
+      name: 'Tablet',
+      added: true,
+    },
+  ]);
 
-
-function handleCheck ({good}) {
-    const upGoods = goods.map( (item) => good.id === item.id? {...item, added: !(item.added)}: item);
-    setGoods(upGoods)
-
-}
-function handleCart (good) {
-    return (
-        
-        <li key={good.id} style= {ulStyle}>
-            <input type='checkbox' checked={good.added} onChange={() => handleCheck({good})}></input>
-           
-            {good.name}
-            
-            <button>Delete</button>
-       
-       </li>
-        
-    )
-
-}
-
-  return (
-    <div>
-        <ul>
-        {goods.map( (good) => handleCart(good) )}
-        </ul>
-    </div>
-  )
+  function handleCheck(good) {
+    const upGoods = goods.map((item) =>
+      good.id === item.id ? { ...item, added: !item.added } : item
+    );
+    setGoods(upGoods);
+  }
+  function handleDelete(good) {
+    const upGoods = goods.filter((item) =>
+      good.id !== item.id
+    );
+    setGoods(upGoods);
   }
 
-export default Cart
+  function handleCart(good) {
+    return (
+      <li key={good.id} style={listItemStyle}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            checked={good.added}
+            onChange={() => handleCheck(good)}
+            style={checkboxStyle}
+          />
+          <span style={{ marginLeft: '40px' }}>{good.name}</span>
+          </div>
+        <button style={deleteButtonStyle} onClick={()=> handleDelete(good)}>Delete</button>
+        
+      </li>
+    );
+  }
+
+  return (
+    <div style={cartStyle}>
+      <ul>{goods.map((good) => handleCart(good))}</ul>
+    </div>
+  );
+}
+
+export default Cart;
